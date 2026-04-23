660b7771c9922e79510f1b1b9c32bca0cc5d558b
r4y3nc <r4y3nc@gmail.com>
2026-04-23 10:41:19 +0700

    feat: add volume dimension to environment model

--- terrain.py
+++ terrain.py
@@ -9,16 +9,63 @@ from OpenGL.GL import *
 from config import TERRAIN_SIZE, TERRAIN_SCALE
 
 
-# ================= LAUT (FLAT) =================
+# ================= LAUT (BALOK AIR / AKUARIUM) =================
 def draw_sea():
-    glDisable(GL_LIGHTING)
-    glColor3f(0.1, 0.4, 0.85)
+    N = TERRAIN_SIZE
+    S = TERRAIN_SCALE
+    base_y = -3.0  
+    
+    min_pos = (0 - N / 2) * S
+    max_pos = (N - 1 - N / 2) * S
 
+    # --- SOLUSI Z-FIGHTING (MICRO-OFFSET) ---
+    eps = -0.0002  # Epsilon: geser 0.02 unit ke luar
+    s_min_x = min_pos - eps
+    s_min_z = min_pos - eps
+    s_max_z = max_pos + eps
+    s_base_y = base_y - eps
+    # ----------------------------------------
+
+    glDisable(GL_LIGHTING)
+
+    # 1. PERMUKAAN ATAS AIR (Gunakan variabel 's_' yang sudah digeser)
+    glColor3f(0.1, 0.4, 0.85)
     glBegin(GL_QUADS)
-    glVertex3f(-50, 0.0, -50)
-    glVertex3f(  0, 0.0, -50)
-    glVertex3f(  0, 0.0,  50)
-    glVertex3f(-50, 0.0,  50)
+    glVertex3f(s_min_x, 0.0, s_min_z) 
+    glVertex3f(0.0,     0.0, s_min_z) 
+    glVertex3f(0.0,     0.0, s_max_z) 
+    glVertex3f(s_min_x, 0.0, s_max_z) 
+    glEnd()
+
+    # 2. DINDING BALOK AIR
+    glColor3f(0.05, 0.25, 0.65)
+    glBegin(GL_QUADS)
+    
+    # Dinding Kiri (X terluar)
+    glVertex3f(s_min_x, 0.0,      s_min_z)
+    glVertex3f(s_min_x, 0.0,      s_max_z)
+    glVertex3f(s_min_x, s_base_y, s_max_z)
+    glVertex3f(s_min_x, s_base_y, s_min_z)
+
+    # Dinding Depan (Z terluar depan)
+    glVertex3f(s_min_x, 0.0,      s_max_z)
+    glVertex3f(0.0,     0.0,      s_max_z)
+    glVertex3f(0.0,     s_base_y, s_max_z)
+    glVertex3f(s_min_x, s_base_y, s_max_z)
+
+    # Dinding Belakang (Z terluar belakang)
+    glVertex3f(s_min_x, 0.0,      s_min_z)
+    glVertex3f(s_min_x, s_base_y, s_min_z)
+    glVertex3f(0.0,     s_base_y, s_min_z)
+    glVertex3f(0.0,     0.0,      s_min_z)
+
+    # Dinding Kanan (Batas tengah dengan tanah)
+    # Ini dibiarkan 0.0 karena posisinya tenggelam di "dalam" tanah
+    glVertex3f(0.0, 0.0,      s_max_z)
+    glVertex3f(0.0, 0.0,      s_min_z)
+    glVertex3f(0.0, s_base_y, s_min_z)
+    glVertex3f(0.0, s_base_y, s_max_z)
+    
     glEnd()
 
 
@@ -124,6 +171,83 @@ class Terrain:
 
         glDisableClientState(GL_VERTEX_ARRAY)
         glDisableClientState(GL_COLOR_ARRAY)
+
+        self.draw_base()
+
+    # ================= FONDASI & DINDING (SKIRTING) =================
+    def draw_base(self):
+        N = self.N
+        S = TERRAIN_SCALE
+        base_y = -3.0  # Kedalaman fondasi pulau
+        
+        # Hitung batas koordinat X dan Z paling ujung
+        min_pos = (0 - N / 2) * S
+        max_pos = (N - 1 - N / 2) * S
+        
+        glDisable(GL_LIGHTING)
+
+        # 1. GAMBAR LANTAI DASAR (WARNA ABU-ABU GELAP)
+        glColor3f(0.2, 0.2, 0.2)
+        glBegin(GL_QUADS)
+        glVertex3f(min_pos, base_y, max_pos)
+        glVertex3f(max_pos, base_y, max_pos)
+        glVertex3f(max_pos, base_y, min_pos)
+        glVertex3f(min_pos, base_y, min_pos)
+        glEnd()
+
+        # 2. GAMBAR DINDING / TEBING TANAH (WARNA COKELAT)
+        glColor3f(0.35, 0.25, 0.15) 
+        glBegin(GL_QUADS)
+
+        # Sisi Belakang (Z = min_pos)
+        for j in range(N - 1):
+            x1 = (j - N / 2) * S
+            x2 = (j + 1 - N / 2) * S
+            z = min_pos
+            y1 = float(self.height[0][j])
+            y2 = float(self.height[0][j+1])
+            glVertex3f(x1, base_y, z)
+            glVertex3f(x2, base_y, z)
+            glVertex3f(x2, y2, z)
+            glVertex3f(x1, y1, z)
+
+        # Sisi Depan (Z = max_pos)
+        for j in range(N - 1):
+            x1 = (j - N / 2) * S
+            x2 = (j + 1 - N / 2) * S
+            z = max_pos
+            y1 = float(self.height[N-1][j])
+            y2 = float(self.height[N-1][j+1])
+            glVertex3f(x1, y1, z)
+            glVertex3f(x2, y2, z)
+            glVertex3f(x2, base_y, z)
+            glVertex3f(x1, base_y, z)
+
+        # Sisi Kiri (X = min_pos)
+        for i in range(N - 1):
+            z1 = (i - N / 2) * S
+            z2 = (i + 1 - N / 2) * S
+            x = min_pos
+            y1 = float(self.height[i][0])
+            y2 = float(self.height[i+1][0])
+            glVertex3f(x, y1, z1)
+            glVertex3f(x, base_y, z1)
+            glVertex3f(x, base_y, z2)
+            glVertex3f(x, y2, z2)
+
+        # Sisi Kanan (X = max_pos)
+        for i in range(N - 1):
+            z1 = (i - N / 2) * S
+            z2 = (i + 1 - N / 2) * S
+            x = max_pos
+            y1 = float(self.height[i][N-1])
+            y2 = float(self.height[i+1][N-1])
+            glVertex3f(x, base_y, z1)
+            glVertex3f(x, y1, z1)
+            glVertex3f(x, y2, z2)
+            glVertex3f(x, base_y, z2)
+
+        glEnd()
 
     # ================= SPAWN UAP =================
     def get_sea_spawn_positions(self, n=10):
